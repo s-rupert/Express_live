ab2493ca8326a8ff07202f666918c34295084bd2
s-rupert <140841484+s-rupert@users.noreply.github.com>
2024-06-11 22:50:32 +0000

    lol

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'pages/index.html'));
+})
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => {
+    console.log(`Server running on https://localhost:${port}`);
+})
